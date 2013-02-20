ece79f5f331976e044421e881fdb98dbdf198112
Jarkko Saltiola <jarkko.saltiola@gmail..com>
2013-02-20 22:43:19 +0200

    Testialustus

---
+++ test_scraper.py
@@ -0,0 +1,23 @@
+import scraper
+import unittest
+
+'''
+http://css.dzone.com/articles/tdd-python-5-minutes
+
+Tiedoston rakenne:
+- Luokka jokaiselle testattavalle funktiolle
+    - Funktio jokaiselle testitapaukselle
+
+### TODO
+- Oma Branch testailua varten
+-
+- ...
+
+
+# This is an empty test-case snippet
+class Test(TestCase):
+    def test_(self):
+        set = Set()
+        self.assertEqual()
+
+'''
