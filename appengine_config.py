929ab2f38d440f2100bc10bc90f6ddcd8f49da8b
Jarkko Saltiola <jarkko.saltiola@gmail..com>
2013-04-10 10:07:47 +0300

    web.py:n siivous

--- appengine_config.py
+++
@@ -1,4 +0,0 @@
-from gaesessions import SessionMiddleware
-def webapp_add_wsgi_middleware(app):
-    app = SessionMiddleware(app, cookie_key="94dc5b30eca02494b9djoicd2q094x54ao4rkuc9av7tyo5tsv4c9")
-    return app
